4880cbe417d3e735a176b0081c499a8d82208918
Blake <bwhite1195@yahoo.com>
2019-01-16 23:29:57 -0500

    completed

---
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -0,0 +1,14 @@
+function alphabetSubsequence(s: string): boolean {
+  for (let i = 0; i < s.length; i++) {
+    if (s.charCodeAt(i) >= s.charCodeAt(i + 1)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(alphabetSubsequence("zab"));
+console.log(alphabetSubsequence("effg"));
+console.log(alphabetSubsequence("cdce"));
+console.log(alphabetSubsequence("ace"));
+console.log(alphabetSubsequence("bxz"));
